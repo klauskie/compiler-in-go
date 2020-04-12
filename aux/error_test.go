25f9c40dc7f2971e42323cd133674b3b7de88a6c
Klaus Kientzle <klauskie19@gmail.com>
2020-04-12 11:26:42 -0600

    added missing testing files

---
+++ aux/error_test.go
@@ -0,0 +1,15 @@
+package aux
+
+import "testing"
+
+func TestNewFoul(t *testing.T) {
+	f := NewFoul(UNKNOWN_TOKEN, 3, "|")
+	if f.Type != UNKNOWN_TOKEN {
+		t.Errorf("NewFoul; Expected Foul of type UNKNOWN (1); got %d", f.Type)
+	}
+
+	f = NewFoul(uint8(2), 3, "|")
+	if f.Message != "error" {
+		t.Errorf("NewFoul; Expected default Foul with message = error; got %s", f.Message)
+	}
+}
